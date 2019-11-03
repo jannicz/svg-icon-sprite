c9c42d818ebce20097d26a3bcd80aabae537ef2c
Suwart, Jan <j.suwart@gmail.com>
2019-11-03 16:53:48 +0100

    Create icon sprite draft based on custom element

--- src/index.ts
+++ src/index.ts
@@ -3,86 +3,84 @@
  * @see https://stackoverflow.com/questions/39037489/extending-htmlelement-constructor-fails-when-webpack-was-used
  */
 class SvgIcon extends HTMLElement {
-    // Specify observed attributes so that
-    // attributeChangedCallback will work
-    static get observedAttributes() {
-        return ['c', 'l'];
+
+  width: string = '100%';
+  height: string = '100%';
+  classes: string;
+  color: string;
+  src: string;
+
+  cssStyle: string = `fill: currentColor;`;
+
+  static get observedAttributes() {
+    return ['width', 'height', 'src', 'classes', 'color'];
+  }
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    console.log('Custom element added to page.');
+    this.render();
+  }
+
+  disconnectedCallback() {
+    console.log('Custom element removed from page.');
+  }
+
+  adoptedCallback() {
+    console.log('Custom element moved to new page.');
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log('Custom element attr changed, name =>', name, 'newValue =>', newValue, '(oldVal =>', oldValue, ')');
+
+    switch (name) {
+      case 'src':
+        this.src = newValue;
+        break;
+      case 'width':
+        this.width = newValue;
+        break;
+      case 'height':
+        this.height = newValue;
+        break;
+      case 'classes':
+        this.classes = newValue;
+        break;
+      case 'color':
+        this.color = newValue;
+        break;
+      default:
+        console.warn('Svg Icon does not support the attribute', name);
     }
 
-    constructor() {
-        super();
+    this.render();
+  }
 
-        const shadow = this.attachShadow({ mode: 'open' });
+  render() {
+    console.log('render..');
 
-        const div = document.createElement('div');
-        const style = document.createElement('style');
-        shadow.appendChild(style);
-        shadow.appendChild(div);
+    if (!this.src) {
+      console.warn('src property is mandatory for SVG Icon');
     }
 
-    connectedCallback() {
-        console.log('Custom svg icon element added to page.');
-        updateStyle(this);
+    if (this.classes) {
+      this.classes.split(' ').forEach((className) => {
+        this.classList.add(className);
+      });
     }
 
-    disconnectedCallback() {
-        console.log('Custom svg icon element removed from page.');
-    }
-
-    adoptedCallback() {
-        console.log('Custom svg icon element moved to new page.');
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-        console.log('Custom square element attributes changed.');
-        updateStyle(this);
-    }
+    this.innerHTML = `
+      <svg width="${this.width}" height="${this.height}" style="${this.cssStyle}">
+        <use xlink:href="${this.src}" style="${this.cssStyle}">
+      </svg>
+    `;
+  }
 }
 
+// Define the custom element as svg-icon
 customElements.define('svg-icon', SvgIcon);
-
-function updateStyle(elem) {
-    console.log('Update element');
-
-    const shadow = elem.shadowRoot;
-    shadow.querySelector('style').textContent = `
-    div {
-      width: ${elem.getAttribute('l')}px;
-      height: ${elem.getAttribute('l')}px;
-      background-color: ${elem.getAttribute('c')};
-    }
-  `;
-}
-
-const add = document.querySelector('.add');
-const update = document.querySelector('.update');
-const remove = document.querySelector('.remove');
-let square;
-
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-add.addEventListener('click', () => {
-    console.log('Add SVG to Dom');
-    // Create a custom square element
-    square = document.createElement('svg-icon');
-    square.setAttribute('l', '100');
-    square.setAttribute('c', 'red');
-    document.body.appendChild(square);
-});
-
-update.addEventListener('click', () => {
-    square.setAttribute('l', random(50, 200));
-    square.setAttribute('c', `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`);
-});
-
-remove.addEventListener('click', () => {
-    document.body.removeChild(square);
-
-    update.setAttribute('disabled', 'disabled');
-    remove.setAttribute('disabled', 'disabled');
-    add.removeAttribute('disabled');
-});
 
 console.log('SVG Icon Sprite init...');
