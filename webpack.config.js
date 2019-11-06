59ce3ac2eb6410cf5432addc4399ad5f70365df7
Suwart, Jan <j.suwart@gmail.com>
2019-11-06 21:39:53 +0100

    Add webpack build task and config

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,31 @@ const path = require('path');
 module.exports = [{
   name: 'default',
   mode: 'development',
-  // context: __dirname + '/src',
   entry: './src/index.ts',
   devtool: 'inline-source-map',
   output: {
+    path: path.resolve(__dirname),
+    filename: 'dist/svg-icon-sprite.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  }
+}, {
+  name: 'prod',
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'svg-icon-sprite.js'
   },
   module: {
     rules: [
@@ -28,7 +47,4 @@ module.exports = [{
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     })
   ]
-}, {
-  name: 'prod',
-  mode: 'production'
 }];
